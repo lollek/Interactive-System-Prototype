814498aa6def9c6e45b7d41d4309f80c1ae2dfd6
Vicel <vicel661@student.liu.se>
2015-02-20 11:57:26 +0100

    Done with circle expandable buttons

--- script/animation_event.js
+++ script/animation_event.js
@@ -1,52 +1,33 @@
-/*
-$(".EditExpandButton").mouseenter(function() {
-	$(".EditExpandButton").css("background-color", "yellow");
-	// on hover
-  $(".circle-container").toggleClass("circle-container-changed");
-  $(".deg135").toggleClass("deg135-changed");
-  $(".deg180").toggleClass("deg180-changed");
-  $(".deg225").toggleClass("deg225-changed");
-  $(".circle-container").toggleClass("circle-container-changed");
-  $(".circle-container").toggleClass("circle-container-changed");
-  $(".circle-container").toggleClass("circle-container-changed");
-});
-$(".EditExpandButton").mouseout(function() {
-  // on mouseout
-  //$(".circle-container-changed").toggleClass("circle-container");
-});
-*/
+var offset = 70; // px
 
-$(".EditExpandButton").mouseenter(function() {
-  // on hover
-  $(".circle-container").toggleClass("circle-resize");
-  $(".deg135").toggleClass("deg135-spanned-out");
-  $(".deg180").toggleClass("deg180-spanned-out");
-  $(".deg225").toggleClass("deg225-spanned-out");
-});
-$(".EditExpandButton").mouseout(function() {
-  // on mouseout
-  $(".circle-container").toggleClass("circle-resize");
-  $(".deg135").toggleClass("deg135-spanned-out");
-  $(".deg180").toggleClass("deg180-spanned-out");
-  $(".deg225").toggleClass("deg225-spanned-out");
-});
+$(".EditExpandButton").hover(function() {
+  var root = $(this).find(".circle-container")
+  var at = 0;
+  root.toggleClass("circle-resize");
 
-
-/*
-function setMouseHover() {
-  $(".EditExpandButton").mouseenter(function() {
-    $(".EditExpandButton").css("background-color", "yellow");
-    // on hover
-    $(".circle-container").toggleClass("circle-container-changed");
-    $("a.center").toggleClass("a.center-changed");
-    $(".deg135").toggleClass("deg135-changed");
-    $(".deg180").toggleClass("deg180-changed");
-    $(".deg225").toggleClass("deg225-changed");
+  root.find("a").each(function() {
+    if(!$(this).hasClass("center"))
+    {
+      var degree = (360/(root.find("a").length -1)) * at
+      at += 1;
+      $(this).css({
+        '-moz-transform': 'rotate(' + degree + 'deg)' + 
+        ' translate(' + offset + 'px)' +
+        ' rotate(-' + degree + 'deg)'});
+    }
   });
-  $(".EditExpandButton").mouseout(function() {
-    // on mouseout
-    $(".EditExpandButton").css("background-color", "white");
-    //$(".circle-container-changed").toggleClass("circle-container");
+}, function() {
+  var root = $(this).find(".circle-container")
+  var at = 0;
+  root.toggleClass("circle-resize");
+
+  root.find("a").each(function() {
+    if(!$(this).hasClass("center"))
+    {
+      $(this).css({
+        '-moz-transform': 'rotate(' + 0 + 'deg)' + 
+        ' translate(' + 0 + 'px)' +
+        ' rotate(-' + 0 + 'deg)'});
+    }
   });
-}
-*/
+});
