b5b3eea3bbd75066495342af2dea3cb112a63609
Vicel <vicel661@student.liu.se>
2015-02-20 09:59:48 +0100

    Javascript for expandable buttons with circle picture transition

--- script/animation_event.js
+++ script/animation_event.js
@@ -1,15 +1,52 @@
-$('.EditExpandButton').mouseenter(function() {
-	$('.EditExpandButton').css('background-color', 'yellow');
+/*
+$(".EditExpandButton").mouseenter(function() {
+	$(".EditExpandButton").css("background-color", "yellow");
 	// on hover
-  $('.circle-container').toggleClass('circle-container-changed');
-  $('.deg135').toggleClass('deg135-changed');
-  $('.deg180').toggleClass('deg180-changed');
-  $('.deg225').toggleClass('deg225-changed');
-  $('.circle-container').toggleClass('circle-container-changed');
-  $('.circle-container').toggleClass('circle-container-changed');
-  $('.circle-container').toggleClass('circle-container-changed');
+  $(".circle-container").toggleClass("circle-container-changed");
+  $(".deg135").toggleClass("deg135-changed");
+  $(".deg180").toggleClass("deg180-changed");
+  $(".deg225").toggleClass("deg225-changed");
+  $(".circle-container").toggleClass("circle-container-changed");
+  $(".circle-container").toggleClass("circle-container-changed");
+  $(".circle-container").toggleClass("circle-container-changed");
 });
 $(".EditExpandButton").mouseout(function() {
   // on mouseout
-  //$('.circle-container-changed').toggleClass('circle-container');
+  //$(".circle-container-changed").toggleClass("circle-container");
 });
+*/
+
+$(".EditExpandButton").mouseenter(function() {
+  // on hover
+  $(".circle-container").toggleClass("circle-resize");
+  $(".deg135").toggleClass("deg135-spanned-out");
+  $(".deg180").toggleClass("deg180-spanned-out");
+  $(".deg225").toggleClass("deg225-spanned-out");
+});
+$(".EditExpandButton").mouseout(function() {
+  // on mouseout
+  $(".circle-container").toggleClass("circle-resize");
+  $(".deg135").toggleClass("deg135-spanned-out");
+  $(".deg180").toggleClass("deg180-spanned-out");
+  $(".deg225").toggleClass("deg225-spanned-out");
+});
+
+
+/*
+function setMouseHover() {
+  $(".EditExpandButton").mouseenter(function() {
+    $(".EditExpandButton").css("background-color", "yellow");
+    // on hover
+    $(".circle-container").toggleClass("circle-container-changed");
+    $("a.center").toggleClass("a.center-changed");
+    $(".deg135").toggleClass("deg135-changed");
+    $(".deg180").toggleClass("deg180-changed");
+    $(".deg225").toggleClass("deg225-changed");
+  });
+  $(".EditExpandButton").mouseout(function() {
+    // on mouseout
+    $(".EditExpandButton").css("background-color", "white");
+    //$(".circle-container-changed").toggleClass("circle-container");
+  });
+}
+*/
