f0afcc27a0e3c44310ab4c077c073ca99698971c
Olle Kvarnström <olle+git@iix.se>
2015-02-23 15:08:04 +0100

    fix spacing

--- js/toolbox.js
+++ js/toolbox.js
@@ -2,13 +2,14 @@ var toolbox = {};
 toolbox.selectedTool = undefined;
 
 toolbox.selectIcon = function(iconName) {
-    toolbox.selectedTool = iconName;
-    switch (iconName) {
-    	case "verticalWall":
-    	    blueprint.addWall(blueprint.VERTICAL);
-    	    break;
-    	case "horizontalWall":
-    	    blueprint.addWall(blueprint.HORIZONTAL);
-    	    break;
-    }
+  toolbox.selectedTool = iconName;
+
+  switch (iconName) {
+    case "verticalWall":
+      blueprint.addWall(blueprint.VERTICAL);
+      break;
+    case "horizontalWall":
+      blueprint.addWall(blueprint.HORIZONTAL);
+      break;
+  }
 };
